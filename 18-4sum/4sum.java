55c107169c9b1ef7c5a2b7ce742e500ac5d96a85
Prajwal1414 <131786284+Prajwal1414@users.noreply.github.com>
2025-05-01 23:15:05 +0530

    Time: 23 ms (17.47%) | Memory: 45 MB (20.03%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        HashSet<List<Integer>> set = new HashSet<>();
+        int n = nums.length;
+        for (int i = 0; i < nums.length - 3; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long)nums[left] + (long) nums[right];
+                    long comp = (long)target - (long)(nums[i] + nums[j]);
+
+                    if (sum == comp) {
+                        set.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[left], nums[right])));
+                        while (left < right && nums[left] == nums[left + 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right - 1])
+                            right--;
+                        left++;
+                        right--;
+                    } else if (sum < comp) {
+                        left++;
+                    } else
+                        right--;
+
+                }
+
+            }
+        }
+        System.out.print(set);
+        for (List<Integer> list : set) {
+            ans.add(list);
+        }
+        return ans;
+    }
+}
