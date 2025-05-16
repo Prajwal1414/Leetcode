95bc757fd3c364d3f46cb857b3bca344b58d6b0b
Prajwal1414 <131786284+Prajwal1414@users.noreply.github.com>
2025-05-16 22:14:11 +0530

    Time: 17 ms (58.95%) | Memory: 48.7 MB (10.34%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,13 +1,15 @@
 import java.util.*;
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        OptionalInt max = Arrays.stream(nums).max();
-        int high = max.getAsInt();
         int low = 1;
+        int high = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            high = Math.max(high, nums[i]);
+        }
 
         while(low <= high){
             int mid = low + (high - low) / 2;
-            int sum = computeSum(nums, threshold, mid);
+            int sum = computeSum(nums, mid);
 
             if(sum <= threshold){
                 high = mid - 1;
@@ -17,10 +19,11 @@ class Solution {
         return low;
     }
 
-    public int computeSum(int[] nums, int threshold, int mid){
+    public int computeSum(int[] nums, int mid){
         int sum = 0;
         for(int i = 0 ; i < nums.length; i++){
-            sum += Math.ceil((float) nums[i] / (float) mid);
+            //This is just an optimal way to write Math.ceil( (float) nums[i] / (float) mid)
+            sum += (nums[i] + mid - 1 ) / mid;
         }
         System.out.println("The sum for mid: " +mid+ " is: " +sum);
         return sum;
